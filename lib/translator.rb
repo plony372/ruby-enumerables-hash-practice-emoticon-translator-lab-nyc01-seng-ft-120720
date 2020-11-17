37d9163b6301216963337407464f2040e468befe
plony benav <menachemfuterfas@gmail.com>
2020-11-17 00:31:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
 # require modules here
+require "yaml"
 
 def load_library
-  # code goes here
+
+gov = YAML.load_file('emoticons.yml')
+
+final_results = gov.each_with_object({}) do |(key, value), final_array|
+final_array[key.to_s] = {:english => value[0], :japanese => value[1]}
 end
+final_results
+end
+
+
+
 
 def get_japanese_emoticon
   # code goes here
